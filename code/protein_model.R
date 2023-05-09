f986df03895d1a1b06ac44f6ea057330e21a1f08
Chetan Raval <chetanbraval@gmail.com>
2023-05-10 07:56:58 +1000

    added test lhs functions

--- code/protein_model.R
+++ code/protein_model.R
@@ -10,6 +10,7 @@
 library(adaptivetau)
 library(tidyverse)
 library(gganimate)
+library(lhs)
 
 # Model parameters
 params <- c(
@@ -24,32 +25,33 @@ params <- c(
 
 # Initial state of the system
 init <- c(
-  RNA_unmutated = 1000,            # 
-  RNA_mutated = 100,
-  Protein_success = 0,
-  Protein_misfolded = 0,
-  Chaperone = 500,
-  Degradation_signal = 50
+  RNA_unmutated = 1000,            # Number of unmutated RNA molecules present
+  RNA_mutated = 100,               # Number of mutated RNA molecules present
+  Protein_success = 0,             # Number of successfully folded proteins
+  Protein_misfolded = 0,           # Number of misfolded proteins
+  Chaperone = 500,                 # Number of chaperone molecules available
+  Degradation_signal = 50          # Initial level of the degradation signal
 )
+
 
 # Propensity function
 propensity_function <- function(state, params, t) {
   return(c(
     # Folding reaction for proteins translated from unmutated RNA
-    params["k_folding_unmutated"] * state["RNA_unmutated"],
+    params[["k_folding_unmutated"]] * state[["RNA_unmutated"]],
     # Folding reaction for proteins translated from mutated RNA
-    params["k_folding_mutated"] * state["RNA_mutated"],
+    params[["k_folding_mutated"]] * state[["RNA_mutated"]],
     # Misfolding reaction for proteins translated from unmutated RNA
-    params["k_misfolding_unmutated"] * state["RNA_unmutated"],
+    params[["k_misfolding_unmutated"]] * state[["RNA_unmutated"]],
     # Misfolding reaction for proteins translated from mutated RNA
-    params["k_misfolding_mutated"] * state["RNA_mutated"],
+    params[["k_misfolding_mutated"]] * state[["RNA_mutated"]],
     # Chaperone-assisted folding reaction for misfolded proteins
-    params["k_chaperone_folding"] * state["Protein_misfolded"] * state["Chaperone"],
+    params[["k_chaperone_folding"]] * state[["Protein_misfolded"]] * state[["Chaperone"]],
     # Protein degradation reaction for misfolded proteins through lysosome-like process
-    params["k_degradation"] * state["Protein_misfolded"] * state["Degradation_signal"],
+    params[["k_degradation"]] * state[["Protein_misfolded"]] * state[["Degradation_signal"]],
     # Negative feedback reaction that increases the degradation signal when
     # there is a high concentration of successfully folded proteins
-    params["feedback_strength"] * state["Protein_success"]^2
+    params[["feedback_strength"]] * state[["Protein_success"]]^2
   ))
 }
 
@@ -82,3 +84,37 @@ result <- ssa.adaptivetau(init.values = init,
 result
 
 result <- as.data.frame(result)
+
+
+# test LHS code - unsure if correct approach
+
+# Define the range for each parameter to test
+param_ranges <- list(
+  k_folding_unmutated = c(0.1, 10),
+  k_folding_mutated = c(0.1, 10),
+  k_misfolding_unmutated = c(0.01, 1),
+  k_misfolding_mutated = c(0.01, 1),
+  k_chaperone_folding = c(0.01, 1),
+  k_degradation = c(0.001, 0.1),
+  feedback_strength = c(0.00001, 0.001)
+)
+
+n_samples <- 100  # Number of samples (adjust as needed)
+
+# Generate normalized LHS samples
+lhs_samples <- randomLHS(n_samples, length(param_ranges))
+
+# Scale the samples to the defined parameter ranges
+param_samples <- matrix(NA, nrow = n_samples, ncol = length(param_ranges),
+                        dimnames = list(NULL, names(param_ranges)))
+for (i in seq_along(param_ranges)) {
+  range_i <- param_ranges[[i]]
+  param_samples[, i] <- range_i[1] + lhs_samples[, i] * (range_i[2] - range_i[1])
+}
+
+t_max <- 100
+results <- vector("list", length = n_samples)
+for (i in 1:n_samples) {
+  params_i <- as.list(param_samples[i, ])
+  results[[i]] <- ssa.adaptivetau(init, transitions, propensity_function, params_i, t_max)
+}
